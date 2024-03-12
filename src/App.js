31d8eb610c08dc65899ffcb5960e9f8b0ed45190
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-12 11:26:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     </div>
     <UseState/>
     <UseEffect/>
+    
     </>
   );
 }
--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,30 +1,24 @@
 import React,{useEffect,useState} from 'react'
 
 export default function UseEffect() {
-//     useEffect(() => {
-//   console.log("render resource type changed")
-
- 
-// },[])
-
-
-const [window, setwindow] = useState(0)    
-const handlesize=()=>{
-    setWindowWidth(window.innerWidth)
-}
-useEffect(() => {
-  window.addEventListener('resize',handlesize)
-  return ()=>{
-    console.log("return from event chnage ")
-  }
-
+  const [count, setcount] = useState(0)
+  const [calc, setcalc] = useState(0)
+  useEffect(() => {
+    setcalc(()=> count*2)
   
-}, [])
+    
+  }, [count])
+  
 
   return (
     <>
     <div><h1>UseEffect</h1></div>
-    <div>{window}</div>
+<p>Count:{count}</p>
+<button onClick={() => setcount((c) => c + 1)}>
+  +
+</button>
+<p>Calculation : {calc}</p>
+    
     
     </>
   )
